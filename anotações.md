fb8a13520a1275a64b9f6838a9c08da0e2a35f0b
Hugo <hug0z_@hotmail.com>
2024-09-16 13:40:41 -0300

    conceito de variáveis e atribuições

--- anotações.md
+++ anotações.md
@@ -4,5 +4,19 @@
 
 
 # Link da AULA
-
+33min
 https://lp.b7web.com.br/aulajs-v
+
+
+# Pesquisa
+
+- Console log do Dev Tools
+- Conceito de variáveis JS e suas atribuições
+  - Diferença entre let, var e const
+  - Diferença entre os tipos de dados que podemos atribuir nas variáveis (Principais, strings, number e booleans)
+  - Outros tipos de variáveis: Arrays e Objetos
+    - Como criar e usar Arrays e objetos?    
+    - Como pegar um item do Array?
+- Concatenações
+
+
---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//Tipos de variáveis
+
+let idade = 34              // Tipo Number
+let nome = "Hugo Hendrix"   // Tipo String
+let logado = true           // Tipo Boolean
+
+// Arrays devem ser abertos utilizando os cochetes, eles aceitam vários tipos de dados e começam com index no zero, 0, 1, 2 e 3
+//Caso seja inserido um número que não tem no array, o console log vai mostrar undefined, ou seja, não foi definida.
+let ingredientes = ["farinha", "água", "sal", "corante"]
+
+//Mostra a quantidade de itens dentro do Array e o seu conteúdo
+console.log(ingredientes)
+
+//Para exibir um item do Array, vai exibir farinha
+console.log(ingredientes[0])
